a786387ca658136b7a5c485a937d6fa71096ae78
luizreboucas <primoneto@gmail.com>
2023-11-03 15:44:29 -0300

    mudanças

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.PersistableBundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity: AppCompatActivity() {
@@ -13,26 +14,43 @@ class MainActivity: AppCompatActivity() {
     lateinit var altura: EditText
     lateinit var peso: EditText
     lateinit var botaoCalcular: Button
+    lateinit var numberResult : TextView
+    lateinit var feedbackMessage : TextView
     override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
         nome.findViewById<EditText>(R.id.idade)
-        altura.findViewById<EditText>(R.id.altura).toString().toDouble()
-        peso.findViewById<EditText>(R.id.peso).toString().toDouble()
+        altura.findViewById<EditText>(R.id.altura)
+        peso.findViewById<EditText>(R.id.peso)
         idade.findViewById<EditText>(R.id.idade)
         botaoCalcular.findViewById<Button>(R.id.btnCalcular)
+        numberResult.findViewById<TextView>(R.id.imcNumberResult)
+        feedbackMessage.findViewById<TextView>(R.id.imcFinalResult)
 
         botaoCalcular.setOnClickListener(View.OnClickListener {
-            var imc = calcularIMC(altura, peso)
+            var altura = altura.text.toString().toDouble()
+            var peso = peso.text.toString().toDouble()
+            var imc: Double = this.calculaIMC(altura, peso)
+            numberResult.setText("Seu IMC é ${imc}")
+            when(imc){
+                in 0.0..18.4 -> feedbackMessage.setText("Abaixo do Peso Normal")
+                in 18.5..24.9 -> feedbackMessage.setText("Peso Normal")
+                in 25.0..29.9 -> feedbackMessage.setText("Excesso de Peso")
+                in 30.0..34.9 -> feedbackMessage.setText("Obesidade Grau I")
+                in 35.0..39.9 -> feedbackMessage.setText("Obesidade Grau II")
+                in 40.0..100.0 -> feedbackMessage.setText("Obesidade Grau III")
+                else -> {
+                    feedbackMessage.setText("Ocorreu um erro, tente novamente!")
+                }
+            }
+
+
         })
 
-
+    }
+    private fun calculaIMC(altura: Double, peso : Double): Double{
+        return peso / (altura * altura)
     }
 
-    private fun calcularIMC(altura: Double, peso: Double){
-        val imc = peso / (altura * altura)
-        when(imc){
-            in 0.0..18.5 ->
 
-        }
-    }
 }
